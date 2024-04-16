e0ef520fddd1a95a140d7e303ca20103eec8d58e
jingyaong <149682199+jingyaong@users.noreply.github.com>
2024-04-16 13:54:38 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-2.cxgc6yes4zut.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Aacs3253"
+customdb = "employee"
+custombucket = "ngjingyao-employee"
+customregion = "us-east-1"
+
